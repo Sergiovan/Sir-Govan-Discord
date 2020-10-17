5fe8453ed485961f064b8ffa67b3d57d0ea4f249
Sergiovan <sergiovanpas@gmail.com>
2020-10-18 01:08:21 +0200

    We're living in a database, whoa-ohhhhh
    Added DB wrapper with a more interesting interface
    Even more type hacks, thanks I hate it
    Can't wait for a ts runtime that checks types tbh

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,10 @@
 import Knex from 'knex';
 
-interface User {
+interface Table {
     rowid: number;
+}
+
+export interface User extends Table {
     id: string;
     name: string;
     discriminator: string;
@@ -14,8 +17,7 @@ interface User {
     option_uninterested: boolean;
 }
 
-interface Puzzle {
-    rowid: number;
+export interface Puzzle extends Table {
     id: string;
     answer: string;
     type: number;
@@ -24,23 +26,20 @@ interface Puzzle {
     winner: number | null;
 }
 
-interface Clue {
-    rowid: number;
+export interface Clue extends Table {
     puzzle_id: number;
     message_id: string;
     content: string;
     created_time: Date;
 }
 
-interface ClueSteal {
-    rowid: number;
+export interface ClueSteal extends Table {
     clue_id: number;
     user_id: number;
-    steal_time: number;
+    steal_time: Date;
 }
 
-interface XpTransaction {
-    rowid: number;
+export interface XpTransaction extends Table {
     user_sender: number | null;
     user_receiver: number | null;
     amount: number;
@@ -48,8 +47,8 @@ interface XpTransaction {
     transaction_time: Date;
 }
 
-type DBName = 'users' | 'puzzles' | 'clues' | 'clue_steals' | 'xp_transactions';
-type DBRecord<T extends DBName> = 
+export type DBName = 'users' | 'puzzles' | 'clues' | 'clue_steals' | 'xp_transactions';
+export type DBRecord<T extends DBName> = 
     T extends 'users' ? User :
     T extends 'puzzles' ? Puzzle :
     T extends 'clues' ? Clue :
@@ -60,7 +59,7 @@ type DBRecord<T extends DBName> =
 // Note: I am unsure of what this actually does, or how to do it better
 // If you are reading this and you do please shoot me a mail or pull request, thx
 type AnyOrUnknownToOther<T1, T2> = unknown extends T1 ? T2 : T1;
-type KnexApproved<T> = Readonly<Readonly<Partial<AnyOrUnknownToOther<Knex.MaybeRawRecord<T>, {}>>>>;
+export type KnexApproved<T> = Readonly<Readonly<Partial<AnyOrUnknownToOther<Knex.MaybeRawRecord<T>, {}>>>>;
 
 
 export class DB {
@@ -86,6 +85,23 @@ export class DB {
 
     async delete<T extends DBName>(table: T, where: Partial<DBRecord<T>>) {
         return await this.table(table).where(where).del();
+    }
+
+    async get<T extends DBName>(table: T, where: Partial<DBRecord<T>>) {
+        return await this.table(table).where(where);
+    }
+
+    async getFirst<T extends DBName>(table: T, where: Partial<DBRecord<T>>) {
+        return await this.table(table).where(where).first();
+    }
+
+    async getLast<T extends DBName>(table: T, where: Partial<DBRecord<T>>) {
+        let elems = await this.table(table).where(where);
+        if (elems.length) {
+            return elems[elems.length - 1];
+        } else {
+            return undefined;
+        }
     }
 
     async select<T extends DBName>(table: T, where: Partial<DBRecord<T>>, select: (keyof DBRecord<T>)[] = []) {
---
+++ src/db_wrapper.ts
@@ -0,0 +1,192 @@
+import Eris from 'eris';
+import * as db from './db';
+
+
+class DBProxy<T extends db.DBName, U extends db.DBRecord<T>> {
+    table: T;
+    rowid: number;
+    db: db.DB;
+    to_commit: Partial<U> = {};
+    
+    constructor(table: T, rowid: number, db: db.DB) {
+        this.table = table;
+        this.rowid = rowid;
+        this.db = db;
+    }
+
+    get(target: U, key: keyof U | 'commit') {
+        if (key === 'commit') {
+            return async () => {
+                await this.db.conn(this.table).where('rowid', this.rowid).update(this.to_commit);
+                this.to_commit = {};
+            };
+        } else {
+            return target[key];
+        }
+    }
+
+    set<V extends keyof U>(target: U, key: V, value: U[V]) {
+        if (key === 'rowid') {
+            throw new Error('Cannot modify rowid');
+        } else {
+            this.to_commit[key] = value;
+            return true;
+        }
+    }
+}
+
+type Committable<T> = T & {commit: () => Promise<void>}
+type MaybeProxy<T> = Promise<Committable<T> | undefined>
+type JustProxy<T> = Promise<Committable<T>>;
+
+class DBWrapper {
+    db: db.DB;
+
+    constructor(database: db.DB) {
+        this.db = database;
+    }
+
+    close() {
+        this.db.close();
+    }
+
+    private proxify<T extends db.DBName, U extends db.DBRecord<T>>(table: T, data: U): Committable<U> {
+        return new Proxy(data, new DBProxy(table, data.rowid, this.db)) as Committable<U>;
+    }
+
+    private async add<T extends db.DBName>(table: T, data: Partial<db.DBRecord<T>>) {
+        let rowid = await this.db.conn(table).insert(data);
+        data.rowid = rowid[0];
+        
+        return this.proxify(table, data as db.DBRecord<T>);
+    }
+
+    async getAllUsers(): Promise<Committable<db.User>[]> {
+        return (await this.db.get('users', {}))
+            .map((e) => new Proxy(e, new DBProxy('users', e.rowid, this.db)) as Committable<db.User>);
+    }
+
+    async addUser(user: Eris.User, member: boolean, nickname: string | null = null): JustProxy<db.User> {
+        let data: Partial<db.User> = {
+            id: user.id,
+            name: user.username,
+            discriminator: user.discriminator,
+            nickname: nickname,
+            avatar: user.avatar ? user.avatarURL : user.defaultAvatarURL,
+            xp: 0,
+            xp_total: 0,
+            level: 0,
+            is_member: member,
+            option_uninterested: false
+        };
+
+        return this.add('users', data);
+    }
+
+    async getUser(user: Eris.User): MaybeProxy<db.User>;
+    async getUser(user: string): MaybeProxy<db.User>;
+    async getUser(user: Eris.User | string): MaybeProxy<db.User> {
+        let id: string;
+        if (user instanceof Eris.User) {
+            id = user.id;
+        } else {
+            id = user;
+        }
+
+        let obj = await this.db.getFirst('users', {id: id});
+        if (obj) {
+            return this.proxify('users', obj);
+        } else {
+            return obj;
+        }
+    }
+
+    async addPuzzle(data: Partial<db.Puzzle>): JustProxy<db.Puzzle> {
+        return this.add('puzzles', data);
+    }
+
+    async getPuzzle(id: string): MaybeProxy<db.Puzzle> {
+        let obj = await this.db.getLast('puzzles', {id: id});
+        if (obj) {
+            return this.proxify('puzzles', obj);
+        } else {
+            return obj;
+        }
+    }
+
+    async addClue(puzzle: string, msg: Eris.Message): JustProxy<db.Clue> {
+        let puzzle_data = await this.getPuzzle(puzzle);
+        if (!puzzle_data) {
+            throw Error(`Puzzle ${puzzle} doesn't exist in database`);
+        }
+        let data: Partial<db.Clue> = {
+            puzzle_id: puzzle_data.rowid,
+            message_id: msg.id,
+            content: msg.content,
+            created_time: new Date(msg.createdAt)
+        };
+        return this.add('clues', data);
+    }
+
+    async getClue(msg: Eris.Message) {
+        let obj = await this.db.getFirst('clues', {message_id: msg.id});
+        if (obj) {
+            return this.proxify('clues', obj);
+        } else {
+            return obj;
+        }
+    }
+
+    async addClueSteal(msg: Eris.Message, user: Eris.User) {
+        let clue_data = await this.getClue(msg);
+        if (!clue_data) {
+            throw Error(`Clue for message ${msg.id} does not exist in database`);
+        }
+        let user_data = await this.getUser(user);
+        if (!user_data) {
+            throw Error(`User ${user.username} does not exist in database`);
+        }
+
+        let data: Partial<db.ClueSteal> = {
+            clue_id: clue_data.rowid,
+            user_id: user_data.rowid,
+            steal_time: new Date()
+        };
+
+        return this.add('clue_steals', data);
+    }
+
+    async transferXP(from: Eris.User | null, to: Eris.User | null, amount: number, reason: number) {
+        if (!from && !to) {
+            throw new Error("Cannot have XP transaction without users");
+        }
+        let from_data, to_data;
+        if (from) {
+            from_data = await this.getUser(from);
+            if (!from_data) {
+                throw Error(`User ${from.username} does not exist in database`);
+            }
+        } else {
+            from_data = null;
+        }
+        if (to) {
+            to_data = await this.getUser(to);
+            if (!to_data) {
+                throw Error(`User ${to.username} does not exist in database`);
+            }
+        } else {
+            to_data = null;
+        }
+
+        let data: Partial<db.XpTransaction> = {
+            user_sender: from_data?.rowid ?? null,
+            user_receiver: to_data?.rowid ?? null,
+            amount: amount,
+            reason: reason,
+            transaction_time: new Date()
+        };
+
+        return this.add('xp_transactions', data);
+    }
+
+};
